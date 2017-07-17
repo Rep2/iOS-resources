bcd74afb10db79cdee9399cac77b0ec0fcb38545
Ivan Rep <ivan.rep2@gmail.com>
2017-07-17 23:47:42 +0200

    Initial Commit

---
+++ iOS resources/iOS resources.h
@@ -0,0 +1,19 @@
+//
+//  iOS resources.h
+//  iOS resources
+//
+//  Created by Ivan Rep on 17/07/2017.
+//  Copyright © 2017 Ivan Rep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOS resources.
+FOUNDATION_EXPORT double iOS_resourcesVersionNumber;
+
+//! Project version string for iOS resources.
+FOUNDATION_EXPORT const unsigned char iOS_resourcesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOS_resources/PublicHeader.h>
+
+
