27c9af7523f76c6742745dc92e1eed2dae5e08e8
Ivan Rep <ivan.rep2@gmail.com>
2017-08-17 16:12:59 +0200

    Better cell handling

--- Helpers/UIKitExtensions/UITableView.swift
+++ Helpers/UIKitExtensions/UITableView.swift
@@ -4,4 +4,12 @@ extension UITableView {
     func registerNib<T: UITableViewCell>(cellType: T.Type) where T: Identifiable {
         register(UINib(nibName: cellType.identifier, bundle: nil), forCellReuseIdentifier: cellType.identifier)
     }
+
+    func cell<T: UITableViewCell>(for indexPath: IndexPath) -> T where T: Identifiable {
+        if let cell = dequeueReusableCell(withIdentifier: T.identifier, for: indexPath) as? T {
+            return cell
+        } else {
+            fatalError("Unable to dequeue cell of type \(T.self) with identifier \(T.identifier)")
+        }
+    }
 }
